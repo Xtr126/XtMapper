f05a71b929bb012627d6fdd25e2e4f726c32e72a
Xtr126 <crawleymsh@gmail.com>
2022-10-06 23:26:46 +0530

    Dpad1Handler: split into declaration and assignment and add if condition

--- app/src/main/java/com/xtr/keymapper/Dpad1Handler.java
+++ app/src/main/java/com/xtr/keymapper/Dpad1Handler.java
@@ -8,31 +8,44 @@ public class Dpad1Handler {
     private final Float radius;
     private final Float xOfCenter;
     private final Float yOfCenter;
+    private final int pointerId = 37;
+    private final String moveUp;
+    private final String moveDown;
+    private final String moveLeft;
+    private final String moveRight;
 
     public Dpad1Handler(String[] data){
         radius = Float.parseFloat(data[0]);
         xOfCenter = Float.parseFloat(data[1]);
         yOfCenter = Float.parseFloat(data[2]);
+
+        moveUp = xOfCenter + " " + Float.sum(yOfCenter, -radius) + " MOVE " + pointerId + "\n";
+        moveDown = xOfCenter + " " + Float.sum(yOfCenter, radius) + " MOVE " + pointerId + "\n";
+        moveLeft = Float.sum(xOfCenter, -radius) + " " + yOfCenter + " MOVE " + pointerId + "\n";
+        moveRight = Float.sum(xOfCenter, radius) + " " + yOfCenter + " MOVE " + pointerId + "\n";
     }
 
     public void sendEvent(String key, String event, DataOutputStream xOut) throws IOException {
-        int pointerId = 37;
+        boolean pointerDown = event.equals("DOWN");
         switch (key){
             case "KEY_UP":{
-
-                xOut.writeBytes(xOfCenter + " " + Float.sum(yOfCenter, -radius) + " MOVE " + pointerId + "\n");
+                if (pointerDown)
+                xOut.writeBytes(moveUp);
                 break;
             }
             case "KEY_DOWN":{
-                xOut.writeBytes(xOfCenter + " " + Float.sum(yOfCenter, radius) + " MOVE " + pointerId + "\n");
+                if (pointerDown)
+                xOut.writeBytes(moveDown);
                 break;
             }
             case "KEY_LEFT":{
-                xOut.writeBytes(Float.sum(xOfCenter, -radius) + " " + yOfCenter + " MOVE " + pointerId + "\n");
+                if (pointerDown)
+                xOut.writeBytes(moveLeft);
                 break;
             }
             case "KEY_RIGHT":{
-                xOut.writeBytes(Float.sum(xOfCenter, radius) + " " + yOfCenter + " MOVE " + pointerId + "\n");
+                if (pointerDown)
+                xOut.writeBytes(moveRight);
                 break;
             }
         }
