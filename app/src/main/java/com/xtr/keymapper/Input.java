35a2b8d07eb09085de9b3222db9829efbe83dcce
Xtr126 <crawleymsh@gmail.com>
2022-10-13 22:45:28 +0530

    TouchPointer: Create inner class for mouse event handling

--- app/src/main/java/com/xtr/keymapper/Input.java
+++ app/src/main/java/com/xtr/keymapper/Input.java
@@ -117,9 +117,9 @@ public class Input {
             injectInputEventMethod = InputManager.class.getMethod(methodName, InputEvent.class, Integer.TYPE);
             initPointers();
             String line;
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("Server: new connection at port: " + MainActivity.DEFAULT_PORT);
-            while ((line = stdInput.readLine()) != null) {
+            while ((line = in.readLine()) != null) {
                 System.out.println(line);
                 String []xy = line.split("\\s+");
                 int pointerId;
