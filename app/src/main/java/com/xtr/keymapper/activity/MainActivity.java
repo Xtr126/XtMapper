4ed3552838b5c52eaf6205615a2a1eea0cb1c0d5
Xtr126 <crawleymsh@gmail.com>
2022-10-20 08:57:23 +0530

    Add DataAdapter for RecyclerView

--- app/src/main/java/com/xtr/keymapper/activity/MainActivity.java
+++ app/src/main/java/com/xtr/keymapper/activity/MainActivity.java
@@ -1,22 +1,33 @@
 package com.xtr.keymapper.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.xtr.keymapper.R;
 import com.xtr.keymapper.Server;
 import com.xtr.keymapper.TouchPointer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_PORT = 6234;
@@ -65,7 +76,13 @@ public class MainActivity extends AppCompatActivity {
     private void initProfilesView(){
         LinearLayout profilesView = findViewById(R.id.profiles_view);
         ImageButton profilesButton = profilesView.findViewById(R.id.profiles);
+
         RecyclerView recyclerView = profilesView.findViewById(R.id.app_grid);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
         Drawable profilesShow = AppCompatResources.getDrawable(this, R.drawable.ic_profiles_1);
         Drawable profilesHide = AppCompatResources.getDrawable(this, R.drawable.ic_profiles_2);
         profilesButton.setOnClickListener(v -> {
@@ -133,4 +150,66 @@ public class MainActivity extends AppCompatActivity {
         pointerOverlay.hideCursor();
         super.onDestroy();
     }
+
+
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+
+        private final ArrayList<RecyclerData> appsDataArrayList = new ArrayList<>();
+
+        public RecyclerViewAdapter() {
+            PackageManager pm = getPackageManager();
+            Intent i = new Intent(Intent.ACTION_MAIN, null);
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            List<ResolveInfo> allApps = pm.queryIntentActivities(i, 0);
+            for(ResolveInfo ri:allApps) {
+                RecyclerData app = new RecyclerData();
+                app.title = ri.loadLabel(pm);
+                app.icon = ri.activityInfo.loadIcon(pm);
+                appsDataArrayList.add(app);
+            }
+        }
+
+        @NonNull
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            // Inflate Layout
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.app_grid, parent, false);
+            return new RecyclerViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+            // Set the data to textview and imageview.
+            RecyclerData recyclerData = appsDataArrayList.get(position);
+            holder.appName.setText(recyclerData.title);
+            holder.appIcon.setImageDrawable(recyclerData.icon);
+        }
+
+        @Override
+        public int getItemCount() {
+            // this method returns the size of recyclerview
+            return appsDataArrayList.size();
+        }
+
+        // View Holder Class to handle Recycler View.
+        private class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+            private final TextView appName;
+            private final ImageView appIcon;
+
+            public RecyclerViewHolder(@NonNull View itemView) {
+                super(itemView);
+                appName = itemView.findViewById(R.id.app_name);
+                appIcon = itemView.findViewById(R.id.app_icon);
+            }
+        }
+
+        private class RecyclerData {
+            private CharSequence title;
+            private Drawable icon;
+        }
+    }
+
 }
