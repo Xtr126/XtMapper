4437d76aad31024f0a2aa92b0ff16ca6eed770d1
Xtr126 <crawleymsh@gmail.com>
2022-10-26 10:40:54 +0530

    Migrate Profiles View to fragment

--- app/src/main/java/com/xtr/keymapper/activity/MainActivity.java
+++ app/src/main/java/com/xtr/keymapper/activity/MainActivity.java
@@ -1,41 +1,21 @@
 package com.xtr.keymapper.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.xtr.keymapper.KeymapConfig;
 import com.xtr.keymapper.R;
 import com.xtr.keymapper.Server;
 import com.xtr.keymapper.TouchPointer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_PORT = 6234;
     public static final int DEFAULT_PORT_2 = 6345;
-    public static final String defaultProfile = "com.xtr.keymapper.default";
-    public String currentProfile;
     public TouchPointer pointerOverlay;
     public Server server;
 
@@ -45,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     private Button keymap;
     private Button configureButton;
     private Button infoButton;
-    private TextView textView;
 
 
     @Override
@@ -76,40 +55,8 @@ public class MainActivity extends AppCompatActivity {
         keymap = findViewById(R.id.start_editor);
         configureButton = findViewById(R.id.config_pointer);
         infoButton = findViewById(R.id.about_button);
-        initProfilesView();
     }
 
-    private void initProfilesView(){
-        LinearLayout profilesView = findViewById(R.id.profiles_view);
-        ImageButton profilesButton = profilesView.findViewById(R.id.profiles);
-
-        textView = findViewById(R.id.profileTextView);
-        RecyclerView recyclerView = profilesView.findViewById(R.id.app_grid);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
-
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-
-        Drawable profilesShow = AppCompatResources.getDrawable(this, R.drawable.ic_profiles_1);
-        Drawable profilesHide = AppCompatResources.getDrawable(this, R.drawable.ic_profiles_2);
-        profilesButton.setOnClickListener(v -> {
-            switch (recyclerView.getVisibility()) {
-                case View.VISIBLE:{
-                    textView.setVisibility(View.GONE);
-                    recyclerView.setVisibility(View.GONE);
-                    profilesButton.setForeground(profilesShow);
-                    break;
-                }
-                case View.GONE:
-                case View.INVISIBLE: {
-                    textView.setVisibility(View.VISIBLE);
-                    recyclerView.setVisibility(View.VISIBLE);
-                    profilesButton.setForeground(profilesHide);
-                    break;
-                }
-            }
-        });
-    }
     private void startService(){
         checkOverlayPermission();
         if(Settings.canDrawOverlays(this)) {
@@ -158,114 +105,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-
-
-    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
-
-        private final ArrayList<RecyclerData> appsDataArrayList = new ArrayList<>();
-        private ColorStateList defaultTint;
-
-        private final ColorStateList selectedTint =
-                ColorStateList.valueOf(getColor(R.color.purple_700));
-
-        private View selectedView;
-
-        private final SharedPreferences sharedPref =
-                getSharedPreferences("settings", MODE_PRIVATE);
-        SharedPreferences.Editor sharedPrefEditor = sharedPref.edit();
-
-
-        public RecyclerViewAdapter() {
-            PackageManager pm = getPackageManager();
-            Intent i = new Intent(Intent.ACTION_MAIN, null);
-            i.addCategory(Intent.CATEGORY_LAUNCHER);
-
-            List<ResolveInfo> allApps = pm.queryIntentActivities(i, 0);
-
-            Drawable drawable = AppCompatResources.getDrawable(MainActivity.this, R.drawable.ic_launcher_foreground);
-            appsDataArrayList.add(
-                    new RecyclerData(defaultProfile,
-                            "Default",
-                            drawable));
-            for(ResolveInfo ri:allApps)
-                appsDataArrayList.add(new RecyclerData(
-                        ri.activityInfo.packageName,
-                        ri.loadLabel(pm),
-                        ri.activityInfo.loadIcon(pm)));
-
-            currentProfile = KeymapConfig.getProfile(MainActivity.this);
-            textView.setText(currentProfile);
-        }
-
-        @NonNull
-        @Override
-        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            // Inflate Layout
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.app_grid, parent, false);
-            defaultTint = view.getRootView().getBackgroundTintList();
-            return new RecyclerViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-            // Set the data to textview and imageview.
-            RecyclerData recyclerData = appsDataArrayList.get(position);
-            holder.appName.setText(recyclerData.title);
-            holder.appIcon.setImageDrawable(recyclerData.icon);
-
-
-            if (recyclerData.packageName.equals(currentProfile) && selectedView == null) {
-                selectedView = holder.appName.getRootView();
-                selectedView.setBackgroundTintList(selectedTint);
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            // this method returns the size of recyclerview
-            return appsDataArrayList.size();
-        }
-
-        // View Holder Class to handle Recycler View.
-        private class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-            private final TextView appName;
-            private final ImageView appIcon;
-
-            public RecyclerViewHolder(@NonNull View itemView) {
-                super(itemView);
-                appName = itemView.findViewById(R.id.app_name);
-                appIcon = itemView.findViewById(R.id.app_icon);
-
-                itemView.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick (View view) {
-                int i = getAdapterPosition();
-                currentProfile = appsDataArrayList.get(i).packageName;
-                textView.setText(currentProfile);
-
-                sharedPrefEditor.putString("profile", currentProfile);
-                sharedPrefEditor.apply();
-
-                selectedView.setBackgroundTintList(defaultTint);
-                view.setBackgroundTintList(selectedTint);
-                selectedView = view;
-            }
-        }
-
-        private class RecyclerData {
-            public RecyclerData(String packageName, CharSequence title, Drawable icon) {
-                this.packageName = packageName;
-                this.icon = icon;
-                this.title = title;
-            }
-
-            String packageName;
-            CharSequence title;
-            Drawable icon;
-        }
-    }
 
 }
