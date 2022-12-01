85b6102ca860278325b17c32d40a4afad941de03
Xtr126 <crawleymsh@gmail.com>
2022-12-01 22:19:39 +0530

    Create InputService

---
+++ app/src/main/java/xtr/keymapper/InputService.java
@@ -0,0 +1,18 @@
+package xtr.keymapper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class InputService extends Service {
+    public InputService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static v
+}
