d538dbf37afbca92cd4267d0b3eb9bce86997726
Xtr126 <crawleymsh@gmail.com>
2023-02-03 07:34:59 +0530

    Add AlertDialog

--- app/src/main/java/xtr/keymapper/EditorService.java
+++ app/src/main/java/xtr/keymapper/EditorService.java
@@ -1,11 +1,14 @@
 package xtr.keymapper;
 
+import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.view.ContextThemeWrapper;
 
@@ -30,7 +33,16 @@ public class EditorService extends Service implements EditorUI.OnHideListener {
                 mService.registerOnKeyEventListener(editor);
             } catch (RemoteException ignored) {
             }
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
+            builder.setMessage(R.string.dialog_alert_editor)
+                    .setPositiveButton("Ok", (dialog, which) -> {})
+                    .setTitle(R.string.dialog_alert_editor_title);
+            AlertDialog dialog = builder.create();
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+            dialog.show();
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
