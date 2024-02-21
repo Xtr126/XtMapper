37e2b3f0aac7ebd2b98514e38494751ddd5921e2
Xtr126 <crawleymsh@gmail.com>
2024-02-21 11:28:56 +0530

    fix: use MaterialAlertDialogBuilder

--- app/src/main/java/xtr/keymapper/editor/EditorUI.java
+++ app/src/main/java/xtr/keymapper/editor/EditorUI.java
@@ -3,7 +3,6 @@ package xtr.keymapper.editor;
 import static xtr.keymapper.keymap.KeymapProfiles.MOUSE_RIGHT;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
@@ -13,6 +12,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -194,7 +197,7 @@ public class EditorUI extends OnKeyEventListener.Stub {
             }
             else if (id == R.id.dpad) {
                 final CharSequence[] items = { "Arrow Keys", "WASD Keys"};
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(context);
                 builder.setTitle("Select Dpad").setItems(items, (dialog, i) -> {
                     if (i == 0) addArrowKeysDpad(defaultX, defaultY);
                     else addWasdDpad(defaultX, defaultY);
